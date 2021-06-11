c151d72674746b34c9417810551c23f2228879a7
nybroe <casper@nybroe.com>
2021-06-11 20:05:45 +0200

    Update order.js

--- cypress/integration/order.js
+++ cypress/integration/order.js
@@ -1,8 +1,8 @@
-const NAME = Cypress.env('NAME') || 'Mattias Siø Fjellvang';
-const AGE = Cypress.env('AGE') || '26';
-const ADDRESS = Cypress.env('ADDRESS') || '';
-const ZIPCITY = Cypress.env('ZIPCITY') || '';
-const PHONE = Cypress.env('PHONE') || '';
+const NAME = Cypress.env('NAME') || 'Mia Bretlau Nybroe';
+const AGE = Cypress.env('AGE') || '28';
+const ADDRESS = Cypress.env('ADDRESS') || 'Bakkesvinget 17';
+const ZIPCITY = Cypress.env('ZIPCITY') || '3550';
+const PHONE = Cypress.env('PHONE') || '31148212';
 
 // stop if test fails
 afterEach(function() {
